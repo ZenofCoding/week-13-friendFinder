a2b16b87f4407e1b2ce02ee2a98f1e0bdcc4e3eb
joe shin <joe.shin@live.com>
2016-07-31 22:52:29 -0400

    added node modules & js code - still has bugs

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the user data.
+// ===============================================================================
+
+//was tablearry
+var userArray = [
+	{
+  "name":"homer",
+  "photo":"http://madbetty.com/wp-content/uploads/2013/11/homer-simpson.jpg",
+  "scores":[
+     5,
+     5,
+     5,
+     5,
+     5,
+     5,
+     5,
+     5,
+     5,
+     5
+   ]
+}
+
+// Note how we export the array. This makes it accessible to other files using require. 
+module.exports = userArray; 
+
